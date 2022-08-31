90affbcaa488b853870bcb3d0eb2ad8b5aa50d38
이재휴 <ijaehyu@ijaehyuui-MacBookPro.local>
2022-09-01 03:03:05 +0900

    removes unnecessary files

--- studio/schemas/utils.js
+++
@@ -1,9 +0,0 @@
-import client from 'part:@sanity/base/client'
-
-export function slugifier(input) {
-  const query = '*[_id == $id][0]'
-  const params = {id: input._ref}
-  return client.fetch(query, params).then(doc => {
-    return doc.title.toLowerCase().replace(/\s+/g, '-').slice(0, 200);
-  });
-}
