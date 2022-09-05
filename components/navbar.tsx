03ddad376426f8f5a6a6790ba6256fe615049d75
이재휴 <ijaehyu@ijaehyuui-MacBookPro.local>
2022-09-06 00:54:55 +0900

    m

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -69,7 +69,7 @@ export function NavBar({categories, selectedCategoryName}
         <Flex justify='center' >
           <div style={{width: '64px', height: '64px'}}>
             <Link href="/"  >
-              <img src='logo.png' style={{width: '64px', height: '64px' }} /> 
+              <img src='reduced_logo.png' style={{width: '64px', height: '64px' }} /> 
             </Link>
           </div>
           <div style={{width: '128px', height: '64px'}}>
